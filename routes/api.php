2069f9811fbb913aba24060ed85a7d49fe03c3bc
Andre Madarang <andre_madarang@hotmail.com>
2020-03-06 00:00:16 -0500

    Add routes for token auth

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +23,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:airlock')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:airlock')->post('/logout', function (Request $request) {
+    $request->user()->tokens()->delete();
+
+    return response('Loggedout', 200);
+});
+
+Route::post('/airlock/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $token = $user->createToken($request->device_name)->plainTextToken;
+
+    $response = [
+        'user' => $user,
+        'token' => $token,
+    ];
+
+    return response($response, 201);
 });
